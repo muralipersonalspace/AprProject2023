c40613b61ca36c3c3cb61550ea26c9653846b435
GITHUB <muraliraj1994@gmail.com>
2023-04-21 21:35:55 +0530

    task done

---
+++ src/org/zoho/Employee.java
@@ -0,0 +1,14 @@
+package org.zoho;
+
+public class Employee {
+       int a =10;	   
+	public void add() {
+		System.out.println(a);
+    	   
+	}
+	
+   public static void main(String[] args) {
+	Employee e = new Employee();
+	e.add();
+}
+}
